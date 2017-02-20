5cbfdc6d092a868ae4b0fadf095174397fbaeeff
hal1932 <yu.arai.19@gmail.com>
2017-02-20 16:48:09 +0900

    サムネイルビューにファイルアイコンを表示

--- file_list/thumbnail_delegate.py
+++ file_list/thumbnail_delegate.py
@@ -13,22 +13,21 @@ class FileThumbnailDelegate(QItemDelegate):
         self.__thumbnail_caches = {}
 
     def sizeHint(self, option, index):
-        return QSize(100, 120)
+        return QSize(100, 130)
 
     def paint(self, painter, option, index):
         path = index.data(Qt.DisplayRole)
 
         if path not in self.__thumbnail_caches:
-            self.__thumbnail_caches[path] = _ThumbnailCache()
-
-        image_rect = qt.resize_rect(option.rect, 100, 100)
-        name_rect = QRect(option.rect.left(), option.rect.top() + 100, 100, 20)
+            self.__thumbnail_caches[path] = _ThumbnailCache(QSize(100, 100))
 
         cache = self.__thumbnail_caches[path]
         if cache.pixmap is None:
             cache.load(path)
 
         if cache.pixmap is not None:
+            image_rect = qt.resize_rect(option.rect, cache.pixmap.size())
+            image_rect = qt.center_rect(option.rect, image_rect)
             painter.drawPixmap(image_rect, cache.pixmap)
         else:
             brush = QBrush(QColor('aliceblue'))
@@ -37,10 +36,12 @@ class FileThumbnailDelegate(QItemDelegate):
             pen.setWidth(0)
             painter.setPen(pen)
 
+            image_rect = qt.resize_rect(option.rect, 100, 100)
             painter.drawRect(image_rect)
 
+        name_rect = QRect(option.rect.left(), option.rect.top() + 100, 100, 30)
         painter.setPen(None)
-        painter.drawText(name_rect, Qt.AlignCenter, os.path.basename(path))
+        painter.drawText(name_rect, Qt.AlignCenter | Qt.AlignBottom, os.path.basename(path))
 
 
 class _ThumbnailCache(object):
@@ -49,24 +50,31 @@ class _ThumbnailCache(object):
     def pixmap(self):
         return self.__pixmap
 
-    def __init__(self):
-        _ThumbnailCache.__static_init__()
+    def __init__(self, size):
+        _ThumbnailCache.__static_init__(size)
         self.__pixmap = None
 
     @staticmethod
-    def __static_init__():
+    def __static_init__(size):
         if _ThumbnailCache.__initialized:
             return
-        _ThumbnailCache.__directory_thumbnail = QPixmap('resources/Folder_64x.png')
+        _ThumbnailCache.__directory_thumbnail = QPixmap('resources/Folder_64x.png').scaled(size)
+        _ThumbnailCache.__thumbnail_size = size
 
     def load(self, path):
         if os.path.isdir(path):
             self.__pixmap = _ThumbnailCache.__directory_thumbnail
         elif os.path.splitext(path)[1].lower() in _ThumbnailCache.__image_exts:
-            self.__pixmap = QPixmap(path)
+            pixmap = QPixmap(path)
+            pixmap_size = qt.fitting_scale_down(pixmap.size(), _ThumbnailCache.__thumbnail_size)
+            self.__pixmap = pixmap.scaled(pixmap_size)
         else:
-            self.__pixmap = None
+            file_info = QFileInfo(path)
+            icon = QFileIconProvider().icon(file_info)
+            size = icon.actualSize(_ThumbnailCache.__thumbnail_size)
+            self.__pixmap = icon.pixmap(size)
 
     __initialized = False
+    __thumbnail_size = None
     __directory_thumbnail = None
     __image_exts = (u'.jpg', u'.png', u'.gif', u'.bmp')
--- lib/qt.py
+++ lib/qt.py
@@ -1,16 +1,52 @@
 # encoding: utf-8
 from PySide.QtCore import *
 
+import math
 
-def move_rect(rect, x, y):
+
+def move_rect(rect, position):
     return QRect(
-        x, y,
+        rect.left(), rect.top(),
         rect.width(), rect.height()
     )
 
 
-def resize_rect(rect, width, height):
+def resize_rect(rect, size):
     return QRect(
         rect.left(), rect.top(),
-        width, height
+        size.width(), size.height()
     )
+
+
+def center_rect(container, target):
+    cw, tw = container.width(), target.width()
+    max_w, min_w = max(cw, tw), min(cw, tw)
+    ofs_w = (max_w - min_w) / float(2)
+    ofs_w = math.copysign(ofs_w, cw - tw)
+
+    ch, th = container.height(), target.height()
+    max_h, min_h = max(ch, th), min(ch, th)
+    ofs_h = (max_h - min_h) / float(2)
+    ofs_h = math.copysign(ofs_h, ch - th)
+
+    return QRect(
+        target.left() + ofs_w, target.top() + ofs_h,
+        tw, th
+    )
+
+
+def fitting_scale_down(container, target):
+    cw, tw = container.width(), target.width()
+    ch, th = container.height(), target.height()
+
+    if cw < tw:
+        scale = (tw - cw) / float(tw)
+        tw = cw
+        th = ch * scale
+
+    if ch < th:
+        scale = (th - ch) / float(th)
+        th = ch
+        tw = cw * scale
+
+    return QSize(tw, th)
