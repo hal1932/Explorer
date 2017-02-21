f5ffaf962bbd78212e2dae3e8228dd4f92389b96
hal1932 <yu.arai.19@gmail.com>
2017-02-21 21:37:06 +0900

    フォルダツリーに右クリックメニューを追加

--- directory_tree/directory_tree_view.py
+++ directory_tree/directory_tree_view.py
@@ -1,7 +1,10 @@
 # encoding: utf-8
 from lib import *
 
+import platform
 import model
+
+import subprocess
 
 
 class DirectoryTreeView(QTreeView):
@@ -16,6 +19,29 @@ class DirectoryTreeView(QTreeView):
         self.clicked.connect(lambda index: self.__on_selected_item(index))
         self.expanded.connect(lambda index: self.model().expand(index))
 
+        self.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.customContextMenuRequested.connect(self.__setup_context_menu)
+
     def __on_selected_item(self, index):
         item = self.model().itemFromIndex(index)
         self.item_selected.emit(item.path)
+
+    def __setup_context_menu(self, point):
+
+        def _open_directory(path):
+            subprocess.call('"{}" "{}"'.format(platform.FILEOPEN_COMMAND, path), shell=True)
+
+        menu_items = {
+            u'フォルダを開く': lambda path: _open_directory(path),
+        }
+
+        menu = QMenu()
+        for label in menu_items.keys():
+            menu.addAction(label)
+
+        executed_action = menu.exec_(self.mapToGlobal(point))
+        action = menu_items[executed_action.text()]
+
+        index = self.currentIndex()
+        item = self.model().itemFromIndex(index)
+        action(item.path)
